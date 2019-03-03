919b92b4987165211d167e2380043d32a50750cb
Tanya Swift <tanyanicolerussi@gmail.com>
2019-03-02 20:14:03 -0700

    User Sign Up Draft 1: n

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,10 +6,11 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", () => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Blocipedia");
         done();
       });
     });
