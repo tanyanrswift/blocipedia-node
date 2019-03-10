8dcb73cb2356df20b133fe73d557526456753566
Tanya Swift <tanyanicolerussi@gmail.com>
2019-03-10 13:30:42 -0600

    User Sign Up Draft 6

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,7 @@
 const User = require("./models").User;
 const bcrypt = require("bcryptjs");
+//const sgMail = require('@sendgrid/mail');
+//sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   createUser(newUser, callback){
@@ -12,6 +14,14 @@ module.exports = {
       password: hashedPassword
     })
     .then((user) => {
+      /*const msg = {
+        to: newUser.email,
+        from: 'test@example.com',
+        subject: 'Confirming your Blocipedia Sign Up',
+        text: 'This is a confirmation email of your sign up with Blocipedia.',
+        html: '<strong>This is a confirmation email of your sign up with Blocipedia.</strong>',
+      };
+      sgMail.send(msg);*/
       callback(null, user);
     })
     .catch((err) => {
