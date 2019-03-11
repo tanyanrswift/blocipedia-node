71828fd25165bd1637a906b801a786ff272d1477
Tanya Swift <tanyanicolerussi@gmail.com>
2019-03-10 20:13:26 -0600

    Wiki Crud Draft 2

--- src/routes/wiki.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const wikiController = require("../controllers/wikiController");
-
-router.get("/wikis/new", wikiController.new);
-router.get("/wikis/create", wikiController.create);
-
-module.exports = router;
