51a05b82f82be0637c0ea08d19c977422d575686
Tanya Swift <tanyanicolerussi@gmail.com>
2019-03-10 18:24:42 -0600

    User Sign In and Out Draft 1

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,5 +7,8 @@ const User = require("../../src/db/models").User;
 
 router.get("/users/sign_up", userController.signUp);
 router.post("/users/sign_up", validation.validateUsers, userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
 
 module.exports = router;
